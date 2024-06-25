641beed84aa3c10abed8cc5a457c6e4ca6be14d2
estluced <estluced@gmail.com>
2024-06-25 21:24:43 +0300

    feat(eslint): init eslint for fe and be

---
+++ packages/eslint/index.js
@@ -0,0 +1,7 @@
+const frontendEslintConfig = require('./fe.config.json')
+const backendEslintConfig = require('./be.config.json')
+
+module.exports = {
+    frontendEslintConfig,
+    backendEslintConfig
+}
